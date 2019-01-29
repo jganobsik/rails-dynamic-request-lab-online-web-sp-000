498d2fdfc76c98236ea6dcedb410a09f340f338a
Joe Ganobsik <jganobsik@gmail.com>
2019-01-29 16:36:51 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,16 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  def first_name
+    self.first_name
+  end
+  
+  def last_name
+    self.last_name
+  end
+  
+  def show
+  @student = Student.find(params[:id])
+  end
 end
